e04189952f0dfb9fdbe7b07e51ed60d025ca960e
Srkinko <46580391+Srkinko@users.noreply.github.com>
2019-09-25 00:03:42 -0400

    Update fetch_data.php

--- final/fetch_data.php
+++ final/fetch_data.php
@@ -1,46 +1,63 @@
 <?php
-$connect = new PDO("mysql:host=localhost;dbname=contact_ManagerDB", "root", "diego6289");
+$connect = new PDO("mysql:host=localhost;dbname=contact_ManagerDB", "phpmyadmin", "poosgroup");
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method == 'GET')
 {
  $data = array(
-  ':FName'   => "%" . $_GET['FName'] . "%",
-  ':LName'   => "%" . $_GET['LName'] . "%",
-  ':Email'   => "%" . $_GET['Email'] . "%",
-  ':HPhone'    => "%" . $_GET['HPhone'] . "%"
+  ':FName'   => $_GET['FName'],
+  ':LName'   => $_GET['LName'],
+  ':Email'   => $_GET['Email'],
+  ':HPhone'    =>$_GET['HPhone']
  );
- $query = "SELECT * FROM Members WHERE FName LIKE :FName AND LName LIKE :LName AND Email LIKE :Email AND HPhone LIKE :HPhone ORDER BY PersonID DESC";
+ 
+ $query = "SELECT * FROM Members JOIN accounts ON Members.UserID = accounts.UserID";
+            
  $statement = $connect->prepare($query);
  $statement->execute($data);
  $result = $statement->fetchAll();
  foreach($result as $row)
  {
   $output[] = array(
-   'PersonID'    => $row['PersonID'],   
-   'FName'  => $row['FName'],
-   'LName'   => $row['LName'],
-   'Email'  => $row['Email'],
-   'HPhone'   => $row['HPhone']
+  //'PersonID'    => $row['PersonID'],
+  'FName'  => $row['FName'],
+  'LName'   => $row['LName'],
+  'Email'  => $row['Email'],
+  'HPhone'   => $row['HPhone']
   );
- }
+}
  header("Content-Type: application/json");
  echo json_encode($output);
 }
 if($method == "POST")
 {
-    $_POST = json_decode(file_get_contents('php://input'), true);
     $data = array(
-  ':FName'  => $_POST['FName'],
-  ':LName'  => $_POST['LName'],
-  ':Email'  => $_POST['Email'],
-  ':HPhone'   => $_POST['HPhone']
- );
-// Figure out how to keep count of person id values.         
- $query = "INSERT INTO Members (PersonID, FName, LName, Email, HPhone) VALUES (59,:FName, :LName, :Email, :HPhone)";
- $statement = $connect->prepare($query);
- $statement->execute($data);
+      ':FName'  => $_POST['FName'],
+      ':LName'  => $_POST['LName'],
+      ':Email'  => $_POST['Email'],
+      ':HPhone'   => $_POST['HPhone']
+     );
+     
+     $query = "INSERT INTO Members (FName, LName, Email, HPhone) VALUES (:FName, :LName, :Email, :HPhone)";
+    
+    //Posts to website
+    $statement = $connect->prepare($query);
+    $statement->execute($data);
+    
+    $data = json_decode(file_get_contents('php://input'), true);
+    
+    //Posts to ARC
+    $statement = $connect->prepare($query);
+    $statement->execute($data);
+
+    header("Content-Type: application/json");
+    
+    //ARC Print
+    echo json_encode($data);
 }
-//
+   
+
+
+
 if($method == 'PUT')
 {
  parse_str(file_get_contents("php://input"), $_PUT);
@@ -52,9 +69,9 @@ if($method == 'PUT')
   ':HPhone'  => $_PUT['HPhone']
  );
  $query = "
- UPDATE Members 
- SET FName = :FName, 
- LName = :LName, 
+ UPDATE Members
+ SET FName = :FName,
+ LName = :LName,
  Email = :Email,
  HPhone = :HPhone
  WHERE PersonID = :PersonID
